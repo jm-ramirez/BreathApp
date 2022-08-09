9b2e42172db7ed234de1aee9ac8f4e379df5fdec
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2022-08-08 23:25:25 -0300

    Agrego libreria "react-native-linear-gradient"
    Agrego libreria "react-native-linear-gradient" para mostrar fondo en degrade, ya que los estilos de react native no soportan linear-gradient.

---
+++ pages/principal/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import style from './styles';
+
+export const Principal = () => {
+    return (
+        <LinearGradient
+            end={{x: 0.0, y: 0.0}} start={{x: 1.0, y: 1.0}}
+            locations={[0,1.0]}
+            colors={['#7B66FF', '#BCBCC7']}
+            style={style.linearGradient}
+        >
+            <Text style={style.title}>
+                Breathe & relax
+            </Text>
+        </LinearGradient>
+    );
+};
---
+++ pages/principal/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  linearGradient: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+  },
+  title: {
+      position: 'absolute',
+      width: '100%',
+      top: '17.57%',
+      fontFamily: 'BRHendrix-Regular',
+      fontWeight:'600',
+      fontSize:20,
+      textAlign:'center',
+      color:'#FFFFFF'
+  }
+});
